ebc1844fa7890fa7501ce90aa3fda5554f35c8b1
Ruan-Steffansom <148716538+Ruan-Steffansom@users.noreply.github.com>
2024-05-02 23:36:06 -0300

    variáveis em JavaScript

--- udemy/javascript/meu_script.js
+++ udemy/javascript/meu_script.js
@@ -1,3 +1,7 @@
 alert('Olá, seja bem vindo ao curso!!!')
 
+/*
+    Selecionar um elemento no DOM 
+    atualizar o valor desse elemento com uma string
+*/
 document.getElementById('nome').value = 'Oi'
