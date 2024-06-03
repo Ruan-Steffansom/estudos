7bdb9aa209fed89a9754996f77d678825c2021d7
Ruan-Steffansom <148716538+Ruan-Steffansom@users.noreply.github.com>
2024-06-03 12:24:39 -0300

    switch

---
+++ curso_em_video/javascript/3_condicoes/ex012_condicoes.js
@@ -0,0 +1,21 @@
+// var hora = 19
+// console.log(`Agora são exatamente ${hora} horas.`)
+// if (hora < 12) {
+//     console.log('Bom Dia!')
+// }else if (hora <= 18) {
+//     console.log('Boa Tarde!')
+// }else {
+//     console.log('Boa Noite!')
+// } 
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora > 4 && hora < 12) {
+    console.log('Bom Dia!')
+}else if (hora >= 12 && hora <= 18) {
+    console.log('Boa Tarde!')
+}else if (hora >= 19 && hora <= 23){
+    console.log('Boa Noite!')
+} else {
+    console.log('Boa Madrugada!')
+}
