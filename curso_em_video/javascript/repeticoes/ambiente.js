16deffd31f8ac98f1a555026680f767a353e2547
Ruan-Steffansom <148716538+Ruan-Steffansom@users.noreply.github.com>
2024-06-05 11:59:51 -0300

    estrutura de repetição

---
+++ curso_em_video/javascript/repeticoes/ambiente.js
@@ -0,0 +1,34 @@
+/*
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+*/
+
+
+
+// while = ENQUANTO 
+// do = FAÇA
+
+/* ESTRUTURA DE REPETICAO COM TESTE LOGICO NO INICIO.
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++ // c+1
+}
+*/
+
+
+
+/* ESTRUTURA DE REPETICAO COM TESTE LOGICO NO INICIO.*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c+1
+} while (c <= 10)
