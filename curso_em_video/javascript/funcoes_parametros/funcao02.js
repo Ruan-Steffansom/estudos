e0c5ce9dde5a8c2590a62556308662cce7f3796a
Ruan-Steffansom <148716538+Ruan-Steffansom@users.noreply.github.com>
2024-10-23 23:35:20 -0300

    .

--- curso_em_video/javascript/funcoes_parametros/funcao02.js
+++ curso_em_video/javascript/funcoes_parametros/funcao02.js
@@ -1,4 +1,4 @@
-function soma(n1=0, n2=0) { // caso algum parâmetro não seja passado, ira considera que é igual a 0
+function soma(n1=0, n2=0) { //(parâmetros pré definidos) caso algum parâmetro não seja passado, ira considera que é igual a 0, isso evitará que o retorno seja NaN.
     return n1 + n2
 }
 console.log(soma(5))
