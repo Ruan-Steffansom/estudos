86993ef5295351bb9759547c9a5082755ec76ac2
Ruan-Steffansom <148716538+Ruan-Steffansom@users.noreply.github.com>
2024-10-24 13:43:52 -0300

    ex functions

--- curso_em_video/javascript/funcoes_parametros/ex/script.js
+++ curso_em_video/javascript/funcoes_parametros/ex/script.js
@@ -3,6 +3,7 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = [] // nosso vetor/array vai receber os dados diditados
 
+//Função para verificar se o número digitado esta entre 1 e 100.
 function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
         return true // não vai escrever nada na tela apenas vai ter um retorno verdadeiro validando o numero em questão.
@@ -11,6 +12,7 @@ function isNumero(n) {
     }
 }
 
+//Função para verificar se o número digitado ja esta na lista.
 function inLista(n, l) {
     if(l.indexOf(Number(n)) != -1) { // se o numero analizado for encontrado na lista.
         return true
@@ -19,18 +21,19 @@ function inLista(n, l) {
     }
 }
 
+//Função para verificar se o número digitado ja esta na lista.
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {// o dado digitado será analisado e só vai ser adicionar na lista se ele for um número e não estiver na lista.
-    valores.push(Number(num.value)) //adiconando valor ao nosso veto/array
-    let item = document.createElement('option') //crinando um elemento 'option' 
-    item.text = `Valor ${num.value} adicionado.` // definindo o tipo do elemento e atribuindo valor a ele
-    lista.appendChild(item) // adicionando nosso elemento a lista
-    res.innerHTML = ''
+    if(isNumero(num.value) && !inLista(num.value, valores)) {// o dado digitado será analisado e só vai ser adicionar na lista se ele for realmente um número e (!)não estiver na lista.
+    valores.push(Number(num.value)) //adiconando valor ao nosso vetor/array.
+    let item = document.createElement('option') //crinando um elemento do tipo 'option'. 
+    item.text = `Valor ${num.value} adicionado.` // definindo que o item terá um valor de texto. 
+    lista.appendChild(item) // adicionando nosso elemento a lista.
+    res.innerHTML = '' //quando adicionar um elemento na lista, vai limpar o resultado (res).
     } else {
     alert('Valor invalido ou ja encontrado na lista.')
     }
-    num.value = '' //limpar o campo sempre que adicionar ou der valor inválido
-    num.focus() //manter o foco do cursor no campo sem precisar clicar.
+    num.value = '' //limpar o campo de input sempre que adicionar ou der valor inválido
+    num.focus() //manter o foco do cursor no campo de input sem precisar clicar.
 }
 
 function finalizar() {
@@ -42,11 +45,15 @@ function finalizar() {
         let menorValor = valores[0]
         let soma = 0
         let media = 0
-        for(let posicao in valores) {
+
+        for(let posicao in valores) { // for(para) ; in (dentro) --> para cada posição dentro de valores.
+
             soma += valores[posicao] //somar os valores adicionados.
             //verificando o maior e o menor número.
+
             if(valores[posicao] > maiorValor)
                 maiorValor = valores[posicao]
+
             if(valores[posicao] < menorValor)
                 menorValor = valores[posicao]
         }
